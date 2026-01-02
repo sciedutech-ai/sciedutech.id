6c95f2d056ed7fbb1e220e0322c884cbbd2bdf77
sciedutech-ai <sciedugame@gmail.com>
2026-01-02 18:29:59 +0700

    Delete js directory

--- js/guard.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "login.html";
-  }
-});
